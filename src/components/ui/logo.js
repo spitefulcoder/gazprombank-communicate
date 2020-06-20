2a9638d3cc0f90c5679eef6cbb132b7c2dfeefc9
god <godik@protonmail.com>
2020-06-20 20:18:41 +0800

    Login component

---
+++ src/components/ui/logo.js
@@ -0,0 +1,9 @@
+import React from "react";
+import logo from "../../assets/blue-logo.svg";
+import "./logo.css";
+
+function Logo() {
+  return <img src={logo} className="logo" alt="Logo" />;
+}
+
+export default Logo;
